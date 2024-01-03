eeb7c3784bcd8b78c2c0f9054fc4f1a33942e4de
mepto <natg.inf@gmail.com>
2024-01-03 11:48:17 +0100

    Fix linting issues

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,18 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='common:index')),
     path('home/', include('common.urls', namespace="common")),
     path('lettings/', include("lettings.urls", namespace="lettings")),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
 ]
 
 handler404 = 'common.views.error_404'
